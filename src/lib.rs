a67fca901f89ae7e62dfc161b45a2c5ca1121896
Jeron Aldaron Lau <aldaronlau@gmail.com>
2022-01-29 00:16:37 -0600

    Update logo and icon for docs.

--- src/lib.rs
+++ src/lib.rs
@@ -80,8 +80,8 @@
 
 #![no_std]
 #![doc(
-    html_logo_url = "https://libcala.github.io/logo.svg",
-    html_favicon_url = "https://libcala.github.io/icon.svg",
+    html_logo_url = "https://ardaku.github.io/mm/logo.svg",
+    html_favicon_url = "https://ardaku.github.io/mm/icon.svg",
     html_root_url = "https://docs.rs/fon"
 )]
 #![deny(unsafe_code)]
